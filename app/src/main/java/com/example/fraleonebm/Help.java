6b6322cc91d61989e057a152edfebd6cdb2a0de9
FraLeone837 <francesco7.leone@polimi.mail.it>
2021-04-06 15:34:29 +0200

    upload project

---
+++ app/src/main/java/com/example/fraleonebm/Help.java
@@ -0,0 +1,25 @@
+package com.example.fraleonebm;
+
+import android.app.Activity;
+import android.content.Context;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.work.BackoffPolicy;
+import androidx.work.Constraints;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.ListenableWorker;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import java.util.concurrent.TimeUnit;
+
+public class Help {
+
+
+}
